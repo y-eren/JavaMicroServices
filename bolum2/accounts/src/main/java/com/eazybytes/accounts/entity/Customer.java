de1d455f43430fad953282a2fad39c0abb6963d8
y-eren <84980503+y-eren@users.noreply.github.com>
2024-09-17 14:19:34 +0300

    Add files via upload

---
+++ bolum2/accounts/src/main/java/com/eazybytes/accounts/entity/Customer.java
@@ -0,0 +1,35 @@
+package com.eazybytes.accounts.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+// table ile uyuşmuyorsa farklı bir class name için table kullanman gerekiyor
+@Entity
+@Getter@Setter@ToString@AllArgsConstructor@NoArgsConstructor
+public class Customer extends BaseEntity{
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="customer_id")
+	private Long customerId;
+	
+	
+	private String name;
+	
+	private String email;
+	
+	@Column(name="mobile_number")
+	private String mobileNumber;
+
+	
+	
+}
